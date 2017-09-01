3db7b821ac32023612cc7cacf7f3549f0becb44e
MaksymKarpenko <karpenko.maxim87gmail.com>
2017-09-01 13:57:47 +0300

    add style to app.scss, add flex-box grid

--- src/features/repositories.js
+++ src/features/repositories.js
@@ -1,9 +1,13 @@
 export default (repo) => `
-	<div class = "repository">
-		<h2 class = "repo_name">${repo.name}<h2>
-		<p class="desc">${repo.description}</p>
-		<p class="updated_date">${repo.date}</p>
-		<p class="language">${repo.language}</p>
+	<div class = "wrapper"">
+		<div class = "row">
+			<div class = "repository col-4">
+				<h2 class = "repo_name">${repo.name}<h2>
+				<p class="desc">${repo.description}</p>
+				<p class="updated_date">${repo.date}</p>
+				<p class="language">${repo.language}</p>
+			</div>
+		</div>
 	</div>
 `
 
